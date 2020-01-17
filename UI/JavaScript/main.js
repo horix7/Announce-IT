ceaa57b0e89359123e49bf67ce5e32ffa3c6235e
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-01-17 00:22:06 -1000

    feat(searchAnnouncement): search an announcement
    -get an announcement by title
    
    [Finishes #170743864]

--- UI/JavaScript/main.js
+++ UI/JavaScript/main.js
@@ -1,4 +1,14 @@
-
+var status=['accepted','declined','active','deactive'];
 function hide(id){
     document.getElementById(id).remove();
+}
+function search(){
+    
+    // console.log(document.getElementById('srch'));
+    if(document.getElementById('srch').value===""){
+        document.getElementById('tbl').style.visibility="hidden";}
+      
+        else{
+        document.getElementById('tbl').style.visibility="visible";
+        }
 }
