cf5f8dfed2d37e03ba316fe142b464b8ca93b24e
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-01-17 01:24:18 -1000

    feat(searchStatusBased): search an announcement
    -get an announcement based on a specific status
    
    [Finishes #170743821]

--- UI/JavaScript/main.js
+++ UI/JavaScript/main.js
@@ -7,8 +7,42 @@ function search(){
     // console.log(document.getElementById('srch'));
     if(document.getElementById('srch').value===""){
         document.getElementById('tbl').style.visibility="hidden";}
-      
+        else if((document.getElementById('srch').value)==="active"){
+            document.getElementById('tbl').style.visibility="hidden";
+            document.getElementById('tbl1').style.visibility="visible"; 
+            document.getElementById('tbl2').style.visibility="hidden";
+            document.getElementById('tbl3').style.visibility="hidden"; 
+            document.getElementById('tbl4').style.visibility="hidden";  
+            }
+            else if((document.getElementById('srch').value)==="accepted"){
+            
+            document.getElementById('tbl2').style.visibility="visible";
+            document.getElementById('tbl').style.visibility="hidden";
+            document.getElementById('tbl1').style.visibility="hidden"; 
+            document.getElementById('tbl3').style.visibility="hidden"; 
+            document.getElementById('tbl4').style.visibility="hidden";    
+                }
+                else if((document.getElementById('srch').value)==="deactive"){
+            
+            document.getElementById('tbl3').style.visibility="visible";
+            document.getElementById('tbl').style.visibility="hidden";
+            document.getElementById('tbl1').style.visibility="hidden"; 
+            document.getElementById('tbl2').style.visibility="hidden";
+            document.getElementById('tbl4').style.visibility="hidden";    
+                    }
+                    else if((document.getElementById('srch').value)==="declined"){
+            
+                        document.getElementById('tbl4').style.visibility="visible";
+                        document.getElementById('tbl').style.visibility="hidden";
+                        document.getElementById('tbl1').style.visibility="hidden"; 
+                        document.getElementById('tbl2').style.visibility="hidden";
+                        document.getElementById('tbl3').style.visibility="hidden";      
+                        }
         else{
         document.getElementById('tbl').style.visibility="visible";
+        document.getElementById('tbl3').style.visibility="hidden";
+        document.getElementById('tbl1').style.visibility="hidden"; 
+        document.getElementById('tbl2').style.visibility="hidden";
+        document.getElementById('tbl4').style.visibility="hidden";  
         }
 }
