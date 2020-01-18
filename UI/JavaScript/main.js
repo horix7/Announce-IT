7c466b1fcfa72dc87613abdb4b57f657bb09e39b
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-01-18 07:34:32 -1000

    feat(login): allow login
    -allow an user admin or advertiser to login the system
    
    [Finishes #170785104]

--- UI/JavaScript/main.js
+++ UI/JavaScript/main.js
@@ -46,3 +46,17 @@ function search(){
         document.getElementById('tbl4').style.visibility="hidden";  
         }
 }
+function login(){
+    if((document.getElementById('email').value === "ugizwenayodiny@gmail.com") && (document.getElementById('psswd').value==="JULIA")){
+        
+        
+        location.replace("../HTML/view-all-announcements-page.html")
+    }
+    else if(document.getElementById('email').value === "" && document.getElementById('psswd').value===""){
+        location.replace("../HTML/login-page.html")
+    }
+    else{
+        location.replace("../HTML/create-announcement-page.html")
+        
+    }
+}
