db0e6dc27b4d3a614133ee9f45192af51e13f472
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-01-20 00:33:35 -1000

    feat(updateStatus):update announcement's status
    -allow an admin to update the status of an announcement
    
    [Finishes #170795355]

--- Server/src/routes/announcementRoutes.js
+++ Server/src/routes/announcementRoutes.js
@@ -6,6 +6,8 @@ import authenticate from '../middleware/authentication';
 const router = express.Router();
 
 router.post('/',authenticate,validateInput(schemas.announceSchema),controller.createAnnouncement);
+router.patch('/:id/sold',authenticate,validateInput(schemas.updateAnnounce),controller.updateStatus);
 router.patch('/:id',authenticate,validateInput(schemas.updateAnnounce),controller.updateAnnouncement);
+
 
 export const announceRouter=router;
